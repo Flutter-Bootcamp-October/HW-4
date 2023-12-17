f2d37887a411d3e8ed0d39da10eee8ab232947c7
Bader Al-Harbi <60719639+badrsh980@users.noreply.github.com>
2023-12-17 23:37:53 +0300

    Bader

--- dart_application_1/bin/routes/main_routes.dart
+++ dart_application_1/bin/routes/main_routes.dart
@@ -9,25 +9,21 @@ class MainRoutes {
   Handler get route {
     final router = Router();
 
-    // Public routes
     router.get("/", rootHandler);
     router.mount(
-        '/admin', AdminRoutes().publicRoute); // No token validation here
+        '/admin', AdminRoutes().publicRoute);
 
-    // Apply token validation middleware to other routes
     final protectedRouter = Router();
-    protectedRouter.mount('/user', UserRoutes().route); // Protected user routes
+    protectedRouter.mount('/user', UserRoutes().route);
     protectedRouter.mount(
-        '/admin', AdminRoutes().protectedRoute); // Protected admin routes
+        '/admin', AdminRoutes().protectedRoute); 
 
     final protectedRoutes = Pipeline()
         .addMiddleware(tokenValidatorMiddleware())
         .addHandler(protectedRouter);
 
-    // Combine public and protected routes
     router.mount("/", protectedRoutes);
 
-    // Catch-all for undefined routes
     router.all("/<ignored|.*>", (Request req) {
       return Response.notFound("Page not found");
     });
